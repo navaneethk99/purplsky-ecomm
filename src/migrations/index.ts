88be918929756cd20b61250f9a6a362de6f80c13
Navaneeth Krishna <navaneethkrishna@Navaneeths-MacBook-Pro.local>
2026-05-20 13:26:26 +0530

    added a shop now button

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260520_065625 from './20260520_065625';
+
+export const migrations = [
+  {
+    up: migration_20260520_065625.up,
+    down: migration_20260520_065625.down,
+    name: '20260520_065625'
+  },
+];
